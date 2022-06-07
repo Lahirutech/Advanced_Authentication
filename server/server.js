6106f1885448756bffd9f7f582ef4d899194b846
Lanka Rathnayake <LankaR@99x.io>
2022-06-07 23:22:39 +0530

    adding cookie parser

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 const profileRoutes = require('./routes/profile')
 const userRoutes=require('./routes/user-routes')
 const mongoose = require("mongoose")
 
+require("dotenv").config({ path: "./config.env" });
+
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 //app.get('/ping', (req, res) => res.send('pong'))
 app.use('/',profileRoutes)
