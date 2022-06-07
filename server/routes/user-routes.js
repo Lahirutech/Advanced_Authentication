af4caf01f55cc3c1ac7b1bce6cde3e5c21bb8dca
Lanka Rathnayake <LankaR@99x.io>
2022-06-07 22:23:50 +0530

    add routes to server.js

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router()
 
-const { signup,login}=require("../controllers/user-controller")
+const { signup,login,refreshToken, verifyToken, getUser,logout}=require("../controllers/user-controller")
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user", verifyToken, getUser);
+router.get("/refresh", refreshToken, verifyToken, getUser);
+router.post("/logout", verifyToken, logout);
+
 
 
 module.exports=router 
