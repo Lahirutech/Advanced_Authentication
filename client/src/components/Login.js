92d1fb046ebe9c0d6dfc87e5a02eee14bb820fdb
Lanka Rathnayake <LankaR@99x.io>
2022-06-09 01:31:07 +0530

    adding the login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,75 @@
-import React from 'react'
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Login() {
+  const history = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const sendRequest = async () => {
+    const res = await axios
+      .post("user/login", {
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // send http request
+    sendRequest()
+      //.then(() => dispatch(authActions.login()))
+      .then(() => history("/user"));
+  };
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          marginLeft="auto"
+          marginRight="auto"
+          width={300}
+          display="flex"
+          flexDirection={"column"}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h2">Login</Typography>
+
+          <TextField
+            name="email"
+            onChange={handleChange}
+            type={"email"}
+            value={inputs.email}
+            variant="outlined"
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            type="password"
+            value={inputs.password}
+            variant="outlined"
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+        </Box>
+      </form>
+    </div>
   )
 }
